da2af914649e488a3d71b3bcdf1c58bd755ea270
JBorchers <79878454+JBorchers@users.noreply.github.com>
2021-08-14 08:21:29 -0600

    added eventCount to app.js

--- src/App.js
+++ src/App.js
@@ -13,25 +13,41 @@ import './nprogress.css';
 class App extends Component {
   state = {
     events: [],
-    locations: []
+    locations: [],
+    NumberOfEvents: 32,
+    defaultEvents: 'all'
   }
   
-  updateEvents = (location) => {
-  getEvents().then((events) => {
-    const locationEvents = (location === 'all') ?
-      events :
-      events.filter((event) => event.location === location);
-    this.setState({
-      events: locationEvents
-    });
-  });
-}
+  updateEvents = (location, eventCount) => {
+		getEvents().then((events) => {
+			const locationEvents = (location === 'all') ? events.slice(0, this.state.numberOfEvents) : 
+			events.filter((event) => event.location === location);
+			if (this.mounted) {
+				this.setState({ 
+					events: locationEvents.slice(0, eventCount),
+					defaultEvents: location
+				});
+			}
+		});
+	}
+
+updateEventsLength(inputValue) {
+		this.setState({
+			numberOfEvents: inputValue
+		});
+		const { defaultEvents } = this.state
+		this.updateEvents(defaultEvents, inputValue);
+	}
 
 componentDidMount() {
+    const { numberOfEvents } = this.state;
     this.mounted = true;
     getEvents().then((events) => {
       if (this.mounted) {
-        this.setState({ events, locations: extractLocations(events) });
+        this.setState({
+          events: events.slice(0, numberOfEvents),
+          locations: extractLocations(events)
+        });
       }
     });
   }
@@ -46,7 +62,7 @@ componentDidMount() {
       <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
       {/* pass state to EventList as prop of events */}
       <EventList events={this.state.events} />
-      <NumberOfEvents />
+      <NumberOfEvents numberOfEvents={this.state.numberOfEvents} updateEventsLength={(value) => this.updateEventsLength(value)} />
       </div>
       );
     }
